044c05e27916e82c5fa5f229ca596c937e870b34
Anne-Marie Esteves <amesteves@octo.com>
2019-04-30 09:49:53 +0200

    add javascript clock exercices

---
+++ javascript/clock/clock.js
@@ -0,0 +1,10 @@
+export const at = (hour) => {
+    const d = new Date()
+    const hour = d.setHours(8)
+    console.log('---- d ---- ', hour))
+
+    const clock = hour.getHours(hour)
+
+
+    return '08:00'
+}
