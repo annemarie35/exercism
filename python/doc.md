92d8030b850ba7e535332e36d35c8041a2071e43
Anne-Marie Esteves <annemarie.esteves@yahoo.fr>
2025-09-17 11:31:02 +0200

    python: protein translation

---
+++ python/doc.md
@@ -0,0 +1,7 @@
+# About Numbers
+Python has three different types of built-in numbers: integers (int), floating-point (float), and complex (complex). Fractions (fractions.Fraction) and Decimals (decimal.Decimal) are also available via import from the standard library.
+
+Whole numbers including hexadecimal (hex()), octal (oct()) and binary (bin()) numbers without decimal places are also identified as ints.
+
+Python fully supports arithmetic between these different number types, and will convert narrower numbers to match their less narrow counterparts when used with the binary arithmetic operators (+, -, *, /, //, and %).
+https://exercism.org/tracks/python/concepts/numbers
---
+++ python/protein-translation/protein_translation.py
@@ -0,0 +1,35 @@
+def proteins(strand):
+    amino_acids_by_codons = {
+        'AUG': 'Methionine',
+        "UUU": 'Phenylalanine',
+        "UUC": 'Phenylalanine',
+        "UUA": 'Leucine',
+        "UUG": 'Leucine',
+        "UCU": 'Serine',
+        "UCC": 'Serine',
+        "UCA": 'Serine',
+        "UCG": 'Serine',
+        "UAU": 'Tyrosine',
+        "UAC": 'Tyrosine',
+        "UGU": 'Cysteine',
+        "UGC": 'Cysteine',
+        "UGG": 'Tryptophan'
+    }
+
+    STOP_CODONS = ['UAA', 'UAG', 'UGA']
+
+    amino_acids_in_rna_sequence = []
+    CODON_LENGTH = 3
+
+    codon_start_position = 0
+    codon_end_position = CODON_LENGTH
+
+    while codon_end_position <= len(strand):
+        codon_to_translate = strand[codon_start_position: codon_end_position]
+        if codon_to_translate in STOP_CODONS:
+            break;
+        amino_acids_in_rna_sequence.append(amino_acids_by_codons[codon_to_translate])
+        codon_start_position = codon_start_position + CODON_LENGTH
+        codon_end_position = codon_end_position + CODON_LENGTH
+
+    return amino_acids_in_rna_sequence
