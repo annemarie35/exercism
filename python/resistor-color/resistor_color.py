28e3898f9a9e9d8cd3e40ac6268a38c85f675fd9
Anne-Marie Esteves <annemarie.esteves@yahoo.fr>
2025-10-08 14:55:12 +0200

    python: resistor color

---
+++ python/resistor-color/resistor_color.py
@@ -0,0 +1,18 @@
+def color_code(color):
+    colors_list = colors()
+
+    return colors_list.index(color)
+
+def colors():
+    return [
+            "black",
+            "brown",
+            "red",
+            "orange",
+            "yellow",
+            "green",
+            "blue",
+            "violet",
+            "grey",
+            "white",
+        ]
